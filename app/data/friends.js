81aa276fb0224918178e13c733d3b837afc0c203
StarLord <chrisannfalk@gmail.com>
2018-08-11 17:04:27 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+// Basic Friends Data
+// NEE TO work on picture links
+var friendsData = [
+    {
+      name: "Tipper Gore",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjFhbCN-ePcAhVn74MKHfwHBisQjRx6BAgBEAU&url=https%3A%2F%2Fwww.askideas.com%2F20-most-funniest-cow-face-pictures-of-all-the-time%2F&psig=AOvVaw3d4gk00iR7ePg8nw2T8Fpy&ust=1534040704776055",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Dip Sheet",
+      photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjE_5ar-ePcAhUF0IMKHRinAcEQjRx6BAgBEAU&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fderp&psig=AOvVaw3h56M5fNcRVKbpJbh35e5L&ust=1534040766345829",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Doofus Rick",
+      photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwik3_zA-ePcAhWH4IMKHQOIBGMQjRx6BAgBEAU&url=https%3A%2F%2Faminoapps.com%2Fc%2Frick-and-morty-amino%2Fpage%2Fitem%2Fdoofus-rick-rick-j19z7%2FZRBN_VeHXI0ed7zr1l050gNlopwg3bWqWG&psig=AOvVaw2VhsnrrZimYL4KgrR8-fqg&ust=1534040811499958",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Pickle Rick",
+      photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-mcvZ-ePcAhVC6IMKHcJyD4sQjRx6BAgBEAU&url=https%3A%2F%2Fstarbaseatlanta.com%2Fproduct%2Frick-and-morty-animated-series-pickle-rick-figure-refrigerator-magnet-mew-unused%2F&psig=AOvVaw1iV-v68TqgnxgXjFljmsbL&ust=1534040858732310",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Maxwell Smart",
+      photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj7343m--PcAhUB4IMKHbvDCDgQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FDon_Adams&psig=AOvVaw0EiHiaVvYWTWdRXXkGpi6k&ust=1534041392417564",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Benny Hill",
+      photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjWoe6o--PcAhWs5oMKHXM2A8gQjRx6BAgBEAU&url=http%3A%2F%2Fwww.entertainmentsouthwales.com%2Fentertainmentsouthwales%2F2018%2F7%2F19%2Fbenny-hill-celebrated-in-play-at-milford-haven&psig=AOvVaw0lbY_WdCUKScR0T0rWSpOt&ust=1534041300319049",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "The Famous Mister Ed",
+      photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjurtGX--PcAhXr34MKHcqoCnIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt0054557%2F&psig=AOvVaw0ElJZQ03YkVG4caKIDPru7&ust=1534041258965311",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
